0601d46666a40aaa93f07c92f84f75b7e560807a
Maciej Kwapisz <ciamek.77@gmail.com>
2025-01-16 23:00:24 +0100

    Add mongodb package with db connection funcs

---
+++ reservation/internal/reservation/mongodb/db.go
@@ -0,0 +1,41 @@
+package mongodb
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDB struct {
+	Collection *mongo.Collection
+}
+
+func Connect(uri string, name string) (*MongoDB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MongoDB{Collection: client.Database(name).Collection(name)}, nil
+}
+
+func (m *MongoDB) Disconnect() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := m.Collection.Database().Client().Disconnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
