427feca1c1d3ed4d534dc592c9df610f041c760c
Maciej Kwapisz <ciamek.77@gmail.com>
2025-01-16 23:06:40 +0100

    Add mock mongo connection for later

---
+++ reservation/internal/reservation/mongodb/mock.go
@@ -0,0 +1,31 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"github.com/benweissmann/memongo"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMock() (*MongoDB, error) {
+	mongoServer, err := memongo.Start("4.2.8")
+	if err != nil {
+		return nil, err
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoServer.URI())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	collection := client.Database("mock").Collection("mock")
+
+	return &MongoDB{Collection: collection}, nil
+}
