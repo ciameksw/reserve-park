800f4da0ac18600b560313b85101a2cd513c5b2d
Maciej Kwapisz <ciamek.77@gmail.com>
2025-03-23 23:00:33 +0100

    Add jwt validation

--- user/internal/user/auth/jwt.go
+++ user/internal/user/auth/jwt.go
@@ -24,10 +24,30 @@ func GenerateJWT(username string, role mongodb.RoleType, key string) (string, er
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	signedJWT, err := token.SignedString(key)
+	signedJWT, err := token.SignedString([]byte(key))
 	if err != nil {
 		return "", err
 	}
 
 	return signedJWT, nil
+}
+
+func ValidateJWT(tokenString string, key string) (*UserClaims, error) {
+	claims := &UserClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(key), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
 }
