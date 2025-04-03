c9b218d01aaa687878fbf099587d3a9f98ff8dc7
Maciej Kwapisz <ciamek.77@gmail.com>
2025-04-03 23:55:17 +0200

    Move code

---
+++ facade/internal/facade/server/common.go
@@ -0,0 +1,13 @@
+package server
+
+import "net/http"
+
+// Helper function to handle errors
+func (s *Server) handleError(w http.ResponseWriter, message string, err error, statusCode int) {
+	if err != nil {
+		s.Logger.Error.Printf("%s: %v", message, err)
+	} else {
+		s.Logger.Error.Println(message)
+	}
+	http.Error(w, message, statusCode)
+}
--- facade/internal/facade/server/handlers_user.go
+++ facade/internal/facade/server/handlers_user.go
@@ -229,13 +229,3 @@ func (s *Server) deleteUserByID(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(resp.StatusCode)
 	io.Copy(w, resp.Body)
 }
-
-// Helper function to handle errors
-func (s *Server) handleError(w http.ResponseWriter, message string, err error, statusCode int) {
-	if err != nil {
-		s.Logger.Error.Printf("%s: %v", message, err)
-	} else {
-		s.Logger.Error.Println(message)
-	}
-	http.Error(w, message, statusCode)
-}
