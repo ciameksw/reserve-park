9cd4f3a9e6cce9ea11ecd83a15914977bedc8932
Maciej Kwapisz <ciamek.77@gmail.com>
2025-04-06 14:22:05 +0200

    Update spot and user services

--- facade/internal/facade/services/spot/spot.go
+++ facade/internal/facade/services/spot/spot.go
@@ -1,6 +1,11 @@
 package spot
 
-import "github.com/ciameksw/reserve-park/facade/internal/facade/config"
+import (
+	"net/http"
+
+	"github.com/ciameksw/reserve-park/facade/internal/facade/config"
+	"github.com/ciameksw/reserve-park/facade/internal/facade/httpclient"
+)
 
 type SpotService struct {
 	SpotURL string
@@ -10,4 +15,94 @@ func NewSpotService(cfg *config.Config) *SpotService {
 	return &SpotService{
 		SpotURL: cfg.SpotURL,
 	}
+}
+
+func (ss *SpotService) GetSpotPrice(r *http.Request) (*http.Response, error) {
+	ct := r.Header.Get("Content-Type")
+	params := httpclient.RequestParams{
+		URL:         ss.SpotURL + "/spots/price",
+		Method:      r.Method,
+		Body:        r.Body,
+		ContentType: &ct,
+	}
+	resp, err := httpclient.SendRequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (ss *SpotService) GetAll(r *http.Request) (*http.Response, error) {
+	ct := r.Header.Get("Content-Type")
+	params := httpclient.RequestParams{
+		URL:         ss.SpotURL + "/spots",
+		Method:      r.Method,
+		Body:        r.Body,
+		ContentType: &ct,
+	}
+	resp, err := httpclient.SendRequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (ss *SpotService) GetSpot(spotID string) (*http.Response, error) {
+	params := httpclient.RequestParams{
+		URL:    ss.SpotURL + "/spots/" + spotID,
+		Method: http.MethodGet,
+	}
+	resp, err := httpclient.SendRequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (ss *SpotService) DeleteSpot(spotID string) (*http.Response, error) {
+	params := httpclient.RequestParams{
+		URL:    ss.SpotURL + "/spots/" + spotID,
+		Method: http.MethodDelete,
+	}
+	resp, err := httpclient.SendRequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (ss *SpotService) AddSpot(r *http.Request) (*http.Response, error) {
+	ct := r.Header.Get("Content-Type")
+	params := httpclient.RequestParams{
+		URL:         ss.SpotURL + "/spots",
+		Method:      r.Method,
+		Body:        r.Body,
+		ContentType: &ct,
+	}
+	resp, err := httpclient.SendRequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (ss *SpotService) EditSpot(r *http.Request) (*http.Response, error) {
+	ct := r.Header.Get("Content-Type")
+	params := httpclient.RequestParams{
+		URL:         ss.SpotURL + "/spots",
+		Method:      r.Method,
+		Body:        r.Body,
+		ContentType: &ct,
+	}
+	resp, err := httpclient.SendRequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
--- facade/internal/facade/services/user/user.go
+++ facade/internal/facade/services/user/user.go
@@ -2,10 +2,10 @@ package user
 
 import (
 	"bytes"
-	"io"
 	"net/http"
 
 	"github.com/ciameksw/reserve-park/facade/internal/facade/config"
+	"github.com/ciameksw/reserve-park/facade/internal/facade/httpclient"
 )
 
 type UserService struct {
@@ -20,14 +20,13 @@ func NewUserService(cfg *config.Config) *UserService {
 
 func (us *UserService) Register(body []byte) (*http.Response, error) {
 	ct := "application/json"
-	params := sendRequestParams{
-		Path:          "/users",
-		Method:        http.MethodPost,
-		Body:          bytes.NewBuffer(body),
-		ContentType:   &ct,
-		Authorization: nil,
+	params := httpclient.RequestParams{
+		URL:         us.UserURL + "/users",
+		Method:      http.MethodPost,
+		Body:        bytes.NewBuffer(body),
+		ContentType: &ct,
 	}
-	resp, err := us.sendRequestToUserService(params)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
@@ -37,14 +36,13 @@ func (us *UserService) Register(body []byte) (*http.Response, error) {
 
 func (us *UserService) Login(r *http.Request) (*http.Response, error) {
 	ct := r.Header.Get("Content-Type")
-	params := sendRequestParams{
-		Path:          "/users/login",
-		Method:        r.Method,
-		Body:          r.Body,
-		ContentType:   &ct,
-		Authorization: nil,
+	params := httpclient.RequestParams{
+		URL:         us.UserURL + "/users/login",
+		Method:      r.Method,
+		Body:        r.Body,
+		ContentType: &ct,
 	}
-	resp, err := us.sendRequestToUserService(params)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
@@ -54,14 +52,13 @@ func (us *UserService) Login(r *http.Request) (*http.Response, error) {
 
 func (us *UserService) Edit(body []byte) (*http.Response, error) {
 	ct := "application/json"
-	params := sendRequestParams{
-		Path:          "/users",
-		Method:        http.MethodPatch,
-		Body:          bytes.NewBuffer(body),
-		ContentType:   &ct,
-		Authorization: nil,
+	params := httpclient.RequestParams{
+		URL:         us.UserURL + "/users",
+		Method:      http.MethodPatch,
+		Body:        bytes.NewBuffer(body),
+		ContentType: &ct,
 	}
-	resp, err := us.sendRequestToUserService(params)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
@@ -71,14 +68,13 @@ func (us *UserService) Edit(body []byte) (*http.Response, error) {
 
 func (us *UserService) GetAll(r *http.Request) (*http.Response, error) {
 	ct := r.Header.Get("Content-Type")
-	params := sendRequestParams{
-		Path:          "/users",
-		Method:        r.Method,
-		Body:          r.Body,
-		ContentType:   &ct,
-		Authorization: nil,
+	params := httpclient.RequestParams{
+		URL:         us.UserURL + "/users",
+		Method:      r.Method,
+		Body:        r.Body,
+		ContentType: &ct,
 	}
-	resp, err := us.sendRequestToUserService(params)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
@@ -87,14 +83,11 @@ func (us *UserService) GetAll(r *http.Request) (*http.Response, error) {
 }
 
 func (us *UserService) GetUser(userID string) (*http.Response, error) {
-	params := sendRequestParams{
-		Path:          "/users/" + userID,
-		Method:        http.MethodGet,
-		Body:          nil,
-		ContentType:   nil,
-		Authorization: nil,
+	params := httpclient.RequestParams{
+		URL:    us.UserURL + "/users/" + userID,
+		Method: http.MethodGet,
 	}
-	resp, err := us.sendRequestToUserService(params)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
@@ -103,14 +96,11 @@ func (us *UserService) GetUser(userID string) (*http.Response, error) {
 }
 
 func (us *UserService) DeleteUser(userID string) (*http.Response, error) {
-	params := sendRequestParams{
-		Path:          "/users/" + userID,
-		Method:        http.MethodDelete,
-		Body:          nil,
-		ContentType:   nil,
-		Authorization: nil,
+	params := httpclient.RequestParams{
+		URL:    us.UserURL + "/users/" + userID,
+		Method: http.MethodDelete,
 	}
-	resp, err := us.sendRequestToUserService(params)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
@@ -119,50 +109,12 @@ func (us *UserService) DeleteUser(userID string) (*http.Response, error) {
 }
 
 func (us *UserService) Authorize(authHeader string) (*http.Response, error) {
-	params := sendRequestParams{
-		Path:          "/users/authorize",
+	params := httpclient.RequestParams{
+		URL:           us.UserURL + "/users/authorize",
 		Method:        http.MethodGet,
-		Body:          nil,
-		ContentType:   nil,
 		Authorization: &authHeader,
 	}
-	resp, err := us.sendRequestToUserService(params)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-}
-
-type sendRequestParams struct {
-	Path          string
-	Method        string
-	Body          io.Reader
-	ContentType   *string
-	Authorization *string
-}
-
-func (us *UserService) sendRequestToUserService(params sendRequestParams) (*http.Response, error) {
-	requestURL := us.UserURL + params.Path
-
-	var body io.Reader
-	if params.Body != nil {
-		body = params.Body
-	}
-	req, err := http.NewRequest(params.Method, requestURL, body)
-	if err != nil {
-		return nil, err
-	}
-
-	if params.ContentType != nil {
-		req.Header.Set("Content-Type", *params.ContentType)
-	}
-	if params.Authorization != nil {
-		req.Header.Set("Authorization", *params.Authorization)
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := httpclient.SendRequest(params)
 	if err != nil {
 		return nil, err
 	}
